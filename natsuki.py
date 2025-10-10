54974bfa311cefbb938967c366da4e8c243fa4a0
MatrixShadeYT <mpro9234@gmail.com>
2025-10-10 08:27:30 -0600

    Works

--- natsuki.py
+++ natsuki.py
@@ -1,38 +1,24 @@
 from tensorflow import keras
-from PIL import Image
 import numpy as np
 
-class imgModel(natsuki):
-    self.type = 'img'
+class imgModel:
     def __init__(self,file=None,scale=128):
-        self.scale = scale
-        self.createModel()
+        self.createModel(scale)
         if file:
             self.load(file)
-    def createModel(self):
+    def createModel(self,scale):
         self.model = keras.Sequential([
-            keras.layers.Input(scale,scale,3),
-            keras.layers.Conv2D(scale/4,(3,3),activation='relu'),
+            keras.layers.Input((scale,scale,3)),
+            keras.layers.Conv2D(32,(3,3),activation='relu'),
             keras.layers.MaxPooling2D(),
             keras.layers.Flatten(),
-            keras.layers.Dense(scale,activation='relu'),
-            keras.layers.Dense(9,activation='sigmoid')
+            keras.layers.Dense(64,activation='relu'),
+            keras.layers.Dense(9,activation='softmax')
         ])
-        self.model.compile(
-            optimizer='adam',
-            loss='categorical_crossentropy',
-            metrics=['accuracy']
-        )
-    def predict(self,img: Image.Image):
-        img = self.parseImage(img)
-        value = self.model.predict(img)[0]
+        #self.model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+    def predict(self,img: np.array):
+        value = self.model.predict(img)
         return value
-    def parseImage(self,img: Image.Image):
-        x = (self.scale,self.scale)
-        img.resize(x)
-        img = np.array(img)/255.0
-        img = np.expand_dims(img,axis=0)
-        return img
     def save(self,filename):
         self.model.save_weights(f"{filename}.h5")
     def load(self,filename):
