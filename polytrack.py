f2f46853c251e258c659713d91e2e04e07fb3d61
MatrixShadeYT <mpro9234@gmail.com>
2025-10-09 23:01:46 -0600

    Simple implication

--- polytrack.py
+++ polytrack.py
@@ -3,35 +3,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from threading import Thread
 from PIL import Image
-import random
 import time
-import os
 import io
 
-if os.path.exists('polytrack.txt'):
-    with open('polytrack.txt','r') as file:
-        moves = [int(i) for i in list(file.readlines()[0])]
-else:
-    moves = '0'
-valuable = f"{random.randint(1000000000000000000000000000000,99999999999999999999999999999999999999999999999999999999999999)}".replace('9',f"{random.randint(0,8)}")
-x = ""
-for i in range(len(moves)):
-    x = f"{x}{moves[i]}"
-choiceMoves = input(f"VALUABLE (type value): {valuable}\nPREVIOUS: {x}\nMOVES (blank/previous): ").replace('value',valuable)
-show = input("SHOW SCREEN (YES/web, NO/save, other/show): ")
-if choiceMoves != "":
-    with open('polytrack.txt','w') as file:
-        file.write(choiceMoves)
-    moves = [int(i) for i in list(choiceMoves)]
-else:
-    moves = [int(i) for i in list(moves)]
 options = Options()
-if show != "yes":
-    options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--window-size=780,580")
 driver = webdriver.Chrome(options=options)
 
+currentKeys = []
 keyIndex = {
     "a": Keys.ARROW_LEFT,
     "d": Keys.ARROW_RIGHT,
@@ -49,11 +31,6 @@ combos = [
     ["d","s","w"]
 ]
 
-def get_image():
-    png_bytes = driver.get_screenshot_as_png()
-    img = Image.open(io.BytesIO(png_bytes)).convert("RGB")
-    return img
-
 def setup_map():
     game = ActionChains(driver)
     game.move_by_offset(600,300).click().pause(0.1)
@@ -61,31 +38,10 @@ def setup_map():
     game.move_by_offset(350,250).click().perform()
     time.sleep(0.5)
 
-def commands(cmds):
-    currentKeys = []
-    actions = ActionChains(driver)
-    for i in range(len(cmds)):
-        currentKeys = currentKeys.copy()
-        if cmds[i] != 0:
-            key = combos[cmds[i]-1]
-            for x in range(len(key)):
-                if not key[x] in currentKeys:
-                    actions.key_down(keyIndex[key[x]])
-                    currentKeys.append(key[x])
-            listed = []
-            for x in range(len(currentKeys)):
-                if not currentKeys[x] in key:
-                    actions.key_up(keyIndex[currentKeys[x]])
-                    listed.append(currentKeys[x])
-            for x in range(len(listed)):
-                currentKeys.remove(listed[x])
-        else:
-            currentKeys = currentKeys.copy()
-            for x in range(len(currentKeys)):
-                actions.key_up(currentKeys[x])
-            currentKeys = []
-        actions.pause(0.2)
-    return actions
+def get_image():
+    png_bytes = driver.get_screenshot_as_png()
+    img = Image.open(io.BytesIO(png_bytes)).convert("RGB")
+    return img
 
 def get_data():
     checkpoints = [i for i in driver.execute_script("return document.getElementsByClassName('checkpoint')[0].children[0].children[1].innerHTML").split('/')]
@@ -98,19 +54,35 @@ def get_data():
         speed += li[length-1][i]*int(val)
     return [checkpoints,speed]
 
-try:
-    driver.get("https://www.yoosfuhl.com/game/polytrack/index.html")
-    time.sleep(3.1)
-    setup_map()
-    time.sleep(2)
-    commands(moves).perform()
-    time.sleep(1)
-    if show == "no":
-        get_image().save("polytrack.png")
-    elif show == "yes":
-        data = [i for i in get_data()]
-        print(f"Checkpoints: {data[0][0]}/{data[0][1]}\nSpeed: {data[1]}")
+def move(num,wait=0.01):
+    combos = [i for i in combos]
+    x = ActionChains(driver)
+    if num == 0:
+        for i in range(len(currentKeys)):
+            x.key_up(currentKeys[i])
     else:
-        get_image().show()
-finally:
-    driver.quit()
+        for i in range(len(currentKeys)):
+            if not currentKeys[i] in combos[num]:
+                x.key_up(currentKeys[i])
+        for i in range(len(combos[num])):
+            if not combos[num][i] in currentKeys:
+                x.key_down(combos[num][i])
+    x.pause(wait).perform()
+
+def system(num,func,*args):
+    t = Thread(target=func,args=(*args))
+    time.sleep(num)
+    t.join()
+
+def runtime(num,func,*args):
+    try:
+        driver.get("https://www.yoosfuhl.com/game/polytrack/index.html")
+        time.sleep(3.1)
+        setup_map()
+        time.sleep(2)
+        system(num,func,*args)
+        time.sleep(1)
+        data = get_data()
+        print(f"Checkpoints: {data[0][0]}/{data[0][1]}\nSpeed: {data[1]}")
+    finally:
+        driver.quit()
